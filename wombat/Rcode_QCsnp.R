efff4435a9bc2526a8deeeeec3871f5bef9a7a8e
Guillermo Martinez-Boggio <102411099+GMBog@users.noreply.github.com>
2025-03-13 10:54:39 -0500

    Update Rcode_QCsnp.R

--- wombat/Rcode_QCsnp.R
+++ wombat/Rcode_QCsnp.R
@@ -1,52 +1,46 @@
-
-
-### Quality control and computation of genomic and microbiome matrices
-### Computation of Linear and Gaussian Kernels for BGLR
-
+# Preparing files for Wombat
+# SNP Quality Control and Computation of Genomic Matrix
 
 library(tidyr)
 library(dplyr)
 
-
-## SNP file should have in the first column animal ID and then SNP markers (one per column)
-## To do that the SNP_in_columns.sh split SNP markers in columns!
-
+# How my SNP data should looks? The SNPfile should have animalID and SNP markers (one per column!)
+# To do that the SNP_in_columns.sh split SNP markers in columns!
 
 ## Read SNP markers 
-
-SNPdata = read.table("~/dirfiles/WOMBAT_FormatFiles/SNPfiles/SNPdata.txt", quote="\"", comment.char="")
+SNPdata <- read.table("~/dirfiles/WOMBAT_FormatFiles/SNPfiles/SNPdata.txt", quote="\"", comment.char="")
 chrmap <- read.csv("~/dirfiles/data_UFUofG/chrmap", sep="")
 
-### Imputation of missing values (3, 4 and 5) with NAs first and then with the mean of each column
+## Imputation of missing values (3, 4 and 5) with NAs first and then with the mean of each column
 SNPdata[SNPdata == 3] <- NA
 SNPdata[SNPdata == 4] <- NA
 SNPdata[SNPdata == 5] <- NA
 
-### Check for NA frequency in each column and remove SNP with higher than 0.05 (SNP call rate 95%) 
+## Check for NA frequency in each column and remove SNP with higher than 0.05 (SNP call rate 95%) 
 MissSNP = colSums(is.na(SNPdata))/nrow(SNPdata)
 table(MissSNP>=0.95)
 
 SNPremove = MissSNP>=0.95
 str(SNPremove)
 
-### Check for NA frequency in each row and remove ANIMAL with higher than 0.05 (ANIMAL call rate 95%) 
+## Check for NA frequency in each row and remove ANIMAL with higher than 0.05 (ANIMAL call rate 95%) 
 MissANI = rowSums(is.na(SNPdata))/ncol(SNPdata)
 table(MissANI>=0.95)
 
 ANIremove = MissANI>=0.95
 str(ANIremove)
 
-### Removing SNP and ANIMALS
+## Removing SNP and ANIMALS
 SNPdata = SNPdata[,!SNPremove]
 SNPdata = SNPdata[!ANIremove,]
 dim(SNPdata)
 
 chrmap = chrmap[!SNPremove,]
 
-### Replace NAs with mean genotype per SNP 
+## Replace NAs with mean genotype per SNP 
 SNP_impdata = SNPdata %>% mutate_all(~ifelse(is.na(.x), round(mean(.x, na.rm = TRUE),0), .x))
 
-### Check for minor allele frequency
+## Check for minor allele frequency
 SNPMAF = SNP_impdata[,-1]
 freq = colSums(SNPMAF)/(2*nrow(SNPMAF))
 snpremove = freq <= 0.01 | freq >= 0.99
@@ -54,50 +48,51 @@ table(snpremove)
 
 chrmap = chrmap[!snpremove,]
 
-
 SNPMAF = SNPMAF[,!snpremove]
 dim(SNPMAF)
 SNP_impdata = cbind(SNP_impdata[1],SNPMAF)
 
-### Order SNP data based on the GenotypeID
+## Order SNP data based on the GenotypeID
 SNP_impdata = SNP_impdata[order(SNP_impdata[,1]),]
 head(SNP_impdata[1])
 
 ##Transpose SNP data (rows = SNPs, cols = Individuals)
 tSNP_impdata = t(SNP_impdata[,-1])
 
-### Save transposed impSNP data
-write.table(tSNP_impdata, file="~/dirfiles/WOMBAT_FormatFiles/SNPfiles/SNPCounts.dat", row.names = F, col.names = F, quote = F)
-write.table(chrmap, file="~/dirfiles/WOMBAT_FormatFiles/SNPfiles/chrmap", row.names = F, col.names = F, quote = F)
+## Save transposed impSNP data
+write.table(tSNP_impdata, file="~/SNPCounts.dat", row.names = F, col.names = F, quote = F)
+write.table(chrmap, file="~/chrmap", row.names = F, col.names = F, quote = F)
 
 
-### Computing G relationship matrix assuming LINEAR kernel
+## Computing G relationship matrix assuming LINEAR kernel
 
-#Read genotypic data
+# Read genotypic data
 X = as.matrix(SNP_impdata[,-1])
 
-##Number of markers
+## Number of markers
 p = ncol(X)
 p
 
-#Scale matrix to center in 0 and variance in 1
+# Scale matrix to center in 0 and variance in 1
 S = scale(X, center =  TRUE, scale = TRUE)
 
-#Compute the G matrix as crossproduct between the scale matrix divided number of markers (S%*%t(S))/p
+# Compute the G matrix as crossproduct between the scale matrix divided number of markers (S%*%t(S))/p
 G = tcrossprod(S)/p
 dim(G)
 
 diag(G) = diag(G)+0.001
 Ginv = solve(G)
 
+# Get the upper triangular matrix
 upper_tri = Ginv[upper.tri(Ginv, diag = TRUE)]
 idx = which(upper.tri(Ginv, diag = TRUE), arr.ind = TRUE)
 upperGinv = cbind(idx, upper_tri)
 
+# Invert G matrix
 det(Ginv)
 
-#Save G matrix
-write.table(upperGinv, file="~/dirfiles/WOMBAT_FormatFiles/SNPfiles/animal.gin", row.names = F, col.names = F, quote = F)
+# Save inverse G matrix (in col format)
+write.table(upperGinv, file="~/animal.gin", row.names = F, col.names = F, quote = F)
 
 
 
